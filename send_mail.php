470284d1bfd10c61b7f7f8f0fe2f4032cb7e06f5
Vitalii MV <agronom9913@gmail.com>
2023-10-08 16:42:27 +0300

    2023.10.08 16-42

--- send_mail.php
+++ send_mail.php
@@ -3,16 +3,17 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require "PHPMailer/src/PHPMailer.php";
     require "PHPMailer/src/Exception.php";
+    require "PHPMailer/src/PHPMailer.php";
 
-    $mail = new PHPMailer(true);
-    $mail->CharSet = "UTF-8";
+    $mail = new PHPMailer(true); /* Создаем объект MAIL */
+    $mail->CharSet = "UTF-8"; /* Задаем кодировку UTF-8 */
+//  $mail->IsHTML(true); /* Разрешаем работу с HTML */
 
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
-    $message = $_POST["message"];
+    $name = $_POST["name"]; /* Принимаем имя пользователя с формы .. */
+    $email = $_POST["email"]; /* Почту */
+    $phone = $_POST["phone"]; /* Телефон */
+    $message = $_POST["message"]; /* Сообщение с формы */
 
     $body = $name.''.$email.''.$phone.''.$message.''.;
     $theme = "[Заявка з форми]";
