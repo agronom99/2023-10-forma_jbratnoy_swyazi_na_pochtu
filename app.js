a603b68b77d5c87fbe287dadba6aeafe70c3e765
Vitalii MV <agronom9913@gmail.com>
2023-10-08 15:39:32 +0300

    2023,10,08

---
+++ app.js
@@ -0,0 +1,53 @@
+const form = document.forms["form"];
+const formArr = Array.from(form);
+const validFormArr = [];
+const button = form.elements["button"];
+
+formArr.forEach((el) => {
+  if (el.hasAttribute("data-reg")) {
+    el.setAttribute("is-valid", "0");
+    validFormArr.push(el);
+  }
+});
+
+form.addEventListener("input", inputHandler);
+button.addEventListener("submit", formCheck);
+
+function inputHandler({ target }) {
+  if (target.hasAttribute("data-reg")) {
+    inputCheck(target);
+  }
+}
+
+function inputCheck(el) {
+  const inputValue = el.value;
+  const inputReg = el.getAttribute("data-reg");
+  const reg = new RegExp(inputReg);
+  if (reg.test(inputValue)) {
+    el.setAttribute("is-valid", "1");
+    el.style.border = "2px solid rgb(0, 196, 0)";
+  } else {
+    el.setAttribute("is-valid", "0");
+    el.style.border = "2px solid rgb(255, 0, 0)";
+  }
+}
+
+function formCheck(e) {
+  e.preventDefault();
+  const allValid = [];
+  validFormArr.forEach((el) => {
+    allValid.push(el.getAttribute("is-valid"));
+  });
+  const isAllValid = allValid.reduce((acc, current) => {
+    return acc && current;
+  });
+
+  if (!Boolean(Number(isAllValid))) {
+    alert("Заповніть поля правильно!");
+    return;
+  }
+  formSubmit();
+}
+function formSubmit() {
+  console.log("Перевірка пройшла, дані відправляються...");
+}
---
+++ send.mail.php
@@ -0,0 +1,26 @@
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require "PHPMailer/src/PHPMailer.php";
+    require "PHPMailer/src/Exception.php";
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = "UTF-8";
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+
+    $body = $name . ' ' . $email . ' ' . $phone . ' ' . $message;
+    $theme = "[Заявка з форми]";
+
+    $mail->addAddress("agronom9913@gmail.com");
+
+    $mail->Subject = $Theme;
+    $mail->Body = $body;
+
+    $mail->send();
+
